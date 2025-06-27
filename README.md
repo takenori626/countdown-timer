c2b6ab4ebbd28f8ff1f3606058b27b8e7ebf1d13
takenori626 <takenori626@gmail.com>
2025-06-27 12:00:08 +0000

    25062721

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # countdown-timer
+
+参考URL
+CSSコードの書き方
+https://webst8.com/code/css-major-basic-property/
+
+タイマー関数
+https://www.geeklibrary.jp/counter-attack/timer-function/#setinterval%25e3%2581%25ae%25e4%25bd%25bf%25e3%2581%2584%25e6%2596%25b9
--- script.js
+++ script.js
@@ -4,7 +4,28 @@ function startCountdown() {
     clearInterval(countdownInterval);//既存のタイマーを止める
 
     const datetimeInput = document.getElementById(`datetime`).value;
-    const 
+    const targetTime = new Date(datetimeInput).getTime();
+
+    if(isNaN(targetTime)) {
+        document.getElementById(`countdown`).textContent = "正しい日付を入力してください";
+        return;
+    }
+    countdownInterval = setInterval(() => {
+        const now = new Date().getTime();
+        const distance = targetTime - now;
+
+        if (distance < 0) {
+            clearInterval(countdownInterval)
+            document
+        }
+    })
+
+
+
+
+
+
+
 
 }
 
