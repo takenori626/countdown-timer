96cd5e25ca378599d3cd489ea3aeb522af3ec69a
takenori626 <takenori626@gmail.com>
2025-06-27 08:55:32 +0000

    25/06/27/1755

---
+++ script.js
@@ -0,0 +1,11 @@
+let countdownInterval;
+
+function startCountdown() {
+    clearInterval(countdownInterval);//既存のタイマーを止める
+
+    const datetimeInput = document.getElementById(`datetime`).value;
+    const 
+
+}
+
+
